111ed63483dc2ea603dcb81aeb206a9a64112fac
waleslau <waleslau@foxmail.com>
2022-08-24 22:36:13 +0800

    update

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
-
-curl -s -L https://github.com/jgm/pandoc/releases/download/2.9.2.1/pandoc-2.9.2.1-linux-amd64.tar.gz | tar xvzf - -C ./
-# npm i -g hexo-cli gulp gulp-uglify
-# npm i
-export PATH="$(pwd)/pandoc-2.9.2.1/bin:$PATH"
-# hexo generate && gulp build
+# ./build.sh
+curl -s -L https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | tar xvzf - -C ./
+export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
 hexo g
