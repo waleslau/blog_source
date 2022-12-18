523ba6b20059af826248dff6c7e12ab5325692ae
waleslau <waleslau@foxmail.com>
2022-12-18 14:57:06 +0800

    配置文件小改

--- build.sh
+++ build.sh
@@ -2,4 +2,5 @@
 # ./build.sh
 curl -s -L https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | tar xvzf - -C ./
 export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
+npm i
 hexo g
