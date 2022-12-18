d97f532cc074acac5f63945a4816b480a994abc5
waleslau <waleslau@foxmail.com>
2022-12-18 16:09:07 +0800

    增加 joplin-blog 相关配置，并发布了两篇旧文

--- build.sh
+++ build.sh
@@ -4,3 +4,9 @@ curl -s -L https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-
 export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
 npm i
 hexo g
+
+# 如果是在本地预览，执行 npm run gen && npm run show 即可
+
+# 如果是在 github actions、cloudflare pages、vercel等平台部署，
+# 把构建命令改成 /build.sh 即可
+# 构建输出目录是 /public
