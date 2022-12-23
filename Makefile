1d4c69b6e3e131d3b26f4fc5cf8de81726861b56
waleslau <waleslau@foxmail.com>
2022-12-24 00:55:30 +0800

    update Makefile, add mdfmt.sh

--- Makefile
+++ Makefile
@@ -4,9 +4,9 @@ default: s
 node_modules: package.json
 	npm install
 
-g: build.sh
+g: node_modules
 	npm run gen
-	for i in `ls source/_posts`;do mdfmt -w source/_posts/$i;done
+	./mdfmt.sh
 	#cat .joplin-cache.json  | python3 -m json.tool > .joplin-cache.f.json
 	cat .joplin-cache.json  | jq > .joplin-cache.f.json
 	mv .joplin-cache.f.json .joplin-cache.json
---
+++ mdfmt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -e "\tdo mdfmt -w posts.md ..."
+for i in $(ls source/_posts); do mdfmt -w source/_posts/$i; done
+
+if [ -s /etc/passwd ] && [ -s /etc/os-release ]; then
+    echo -e "\tYou are use Linux now."
+else
+    echo -e "\tYou are use Widows now."
+    echo -e "\tLF --> CRLF"
+    for i in $(ls source/_posts); do sed -i 's/$/\r/g' source/_posts/$i; done
+fi
