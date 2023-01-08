0dcf7f2f446c2433e1474bb4072b539b3aa7a693
waleslau <waleslau@foxmail.com>
2023-01-08 15:55:45 +0800

    adjust workflow
    new file:   .github/workflows/build.yml
    modified:   .gitignore
    modified:   build.sh
    modified:   mdfmt.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,70 @@
 #!/bin/bash
 
-# install pandoc
-curl -s -L https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | tar xvzf - -C ./
-cat /etc/os-release
-export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
-echo 'IF9fX19fX19fX19fX19fX19fX19fX19fXyAKPCBkb3dubG9hZCBwYW5kb2MgLi4uIE9LID4KIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSAKICAgICAgICBcICAgXl9fXgogICAgICAgICBcICAob28pXF9fX19fX18KICAgICAgICAgICAgKF9fKVwgICAgICAgKVwvXAogICAgICAgICAgICAgICAgfHwtLS0tdyB8CiAgICAgICAgICAgICAgICB8fCAgICAgfHwK' | base64 -d
+# init pandoc and mdfmt
+if test -s /etc/passwd && test -s /etc/os-release; then
+    echo "You are use Linux now."
+    # install pandoc
+    if $(test -s $(pwd)/pandoc-2.19.2/bin/pandoc) || $(command -v pandoc >/dev/null 2>&1); then
+        echo 'already have pandoc here.'
+    else
+        echo -e "\e[36m install pandoc \e[0m"
+        curl -s -L https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | tar xvzf - -C ./
+        # set $PATH
+        export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
+    fi
+    
+    # install mdfmt
+    if $(test -s $(pwd)/pandoc-2.19.2/bin/mdfmt) || $(command -v mdfmt >/dev/null 2>&1); then
+        echo 'already have mdfmt here.'
+    else
+        echo -e "\e[36m install mdfmt \e[0m"
+        mkdir $(pwd)/mdfmt && curl -s -L https://github.com/elliotxx/mdfmt/releases/download/v0.4.2/mdfmt_0.4.2_Linux_x86_64.tar.gz | tar xvzf - -C ./mdfmt/ && mv ./mdfmt/mdfmt ./pandoc-2.19.2/bin/ && rm -rf ./mdfmt
+        # set $PATH
+        export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
+    fi
+else
+    echo "You are use Windows now."
+fi
 
-install mdfmt
-mkdir $(pwd)/mdfmt && curl -s -L https://github.com/elliotxx/mdfmt/releases/download/v0.4.2/mdfmt_0.4.2_Linux_x86_64.tar.gz | tar xvzf - -C ./mdfmt/ && mv ./mdfmt/mdfmt ./pandoc-2.19.2/bin/
+# init nodejs-lts
+if test -s /etc/passwd && test -s /etc/os-release && test ! -d $HOME/.nvm; then
+    echo -e "\e[36m init nodejs-lts \e[0m"
+    curl -o- https://cdn.jsdelivr.net/gh/nvm-sh/nvm/install.sh | bash
+    export NVM_DIR="$HOME/.nvm"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+    [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+    nvm install --lts
+    nvm use --lts
+    nvm alias default lts/*
+else
+    echo "already have nvm here, or you are use Widows now."
+fi
+
+if $(command -v pnpm >/dev/null 2>&1); then
+    echo 'already have pnpm here.'
+else
+    npm install -g pnpm
+    pnpm install
+fi
+
+# gen
+
+curl http://127.0.0.1:41184 >/dev/null 2>&1 && echo -e "\e[36m pnpm mami ... \e[0m" && pnpm mami
+
+# mdfmt
+echo -e "\e[36m do mdfmt ... \e[0m"
 for i in $(ls source/_posts); do mdfmt -w source/_posts/$i; done
-echo 'IF9fX19fX19fX19fX19fX19fX19fX18gCjwgZm9ybWF0IHRoZSAubWQgZmlsZS4gPgogLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSAKICAgICAgICBcICAgXl9fXgogICAgICAgICBcICAob28pXF9fX19fX18KICAgICAgICAgICAgKF9fKVwgICAgICAgKVwvXAogICAgICAgICAgICAgICAgfHwtLS0tdyB8CiAgICAgICAgICAgICAgICB8fCAgICAgfHwK' | base64 -d
 
-./mdfmt.sh
+if test -s /etc/passwd && test -s /etc/os-release; then
+    echo "You are use Linux now."
+else
+    echo -e "\e[36m Widows: LF --> CRLF \e[0m"
+    for i in $(ls source/_posts); do sed -i 's/$/\r/g' source/_posts/$i; done
+fi
 
-# build
-npm install pnpm
-pnpm install
+# build blog
+echo -e "\e[36m build blog \e[0m"
 export TZ='Asia/Shanghai'
-echo 'IF9fX19fX19fX19fX19fX19fX19fX19fX19fXyAKPCBleHBvcnQgVFo9J0FzaWEvU2hhbmdoYWknID4KIC0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLS0tLSAKICAgICAgICBcICAgXl9fXgogICAgICAgICBcICAob28pXF9fX19fX18KICAgICAgICAgICAgKF9fKVwgICAgICAgKVwvXAogICAgICAgICAgICAgICAgfHwtLS0tdyB8CiAgICAgICAgICAgICAgICB8fCAgICAgfHwK' | base64 -d
 pnpm hexo generate
 
 # 如果是在 github actions、cloudflare pages、vercel等平台部署，
--- mdfmt.sh
+++ mdfmt.sh
@@ -3,9 +3,8 @@ echo -e "\e[36m do mdfmt -w posts.md ... \e[0m"
 for i in $(ls source/_posts); do mdfmt -w source/_posts/$i; done
 
 if test -s /etc/passwd && test -s /etc/os-release; then
-    echo -e "\e[36m You are use Linux now. \e[0m"
+    echo "You are use Linux now."
 else
-    echo -e "\e[36m You are use Widows now. \e[0m"
-    echo -e "\e[36m LF --> CRLF"
+    echo -e "\e[36m Widows: LF --> CRLF \e[0m"
     for i in $(ls source/_posts); do sed -i 's/$/\r/g' source/_posts/$i; done
 fi
