3dff81f20f8b5565ec426ecfb621ae551501a114
waleslau <waleslau@foxmail.com>
2023-04-01 10:36:24 +0800

    Revert "update"
    This reverts commit bd7d632074042a6a62c5a797e14141211d0ddd81.

--- build.sh
+++ build.sh
@@ -2,49 +2,64 @@
 
 # init pandoc and mdfmt
 if [ "$(uname -s)" = "Linux" ]; then
-	echo "You are use Linux now."
-	# install pandoc
-	if $(test -s $(pwd)/pandoc-2.19.2/bin/pandoc) || hash pandoc 2>/dev/null; then
-		echo 'already have pandoc here.'
-	else
-		echo -e "\e[36m install pandoc \e[0m"
-		curl -s -L https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | tar xvzf - -C ./
-		# set $PATH
-		export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
-	fi
-
-	# install mdfmt
-	if $(test -s $(pwd)/pandoc-2.19.2/bin/mdfmt) || hash mdfmt 2>/dev/null; then
-		echo 'already have mdfmt here.'
-	else
-		echo -e "\e[36m install mdfmt \e[0m"
-		mkdir $(pwd)/mdfmt && curl -s -L https://github.com/elliotxx/mdfmt/releases/download/v0.4.2/mdfmt_0.4.2_Linux_x86_64.tar.gz | tar xvzf - -C ./mdfmt/ && mv ./mdfmt/mdfmt ./pandoc-2.19.2/bin/ && rm -rf ./mdfmt
-		# set $PATH
-		export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
-	fi
+    echo "You are use Linux now."
+    # install pandoc
+    if $(test -s $(pwd)/pandoc-2.19.2/bin/pandoc) || hash pandoc 2>/dev/null; then
+        echo 'already have pandoc here.'
+    else
+        echo -e "\e[36m install pandoc \e[0m"
+        curl -s -L https://github.com/jgm/pandoc/releases/download/2.19.2/pandoc-2.19.2-linux-amd64.tar.gz | tar xvzf - -C ./
+        # set $PATH
+        export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
+    fi
+    
+    # install mdfmt
+    if $(test -s $(pwd)/pandoc-2.19.2/bin/mdfmt) || hash mdfmt 2>/dev/null; then
+        echo 'already have mdfmt here.'
+    else
+        echo -e "\e[36m install mdfmt \e[0m"
+        mkdir $(pwd)/mdfmt && curl -s -L https://github.com/elliotxx/mdfmt/releases/download/v0.4.2/mdfmt_0.4.2_Linux_x86_64.tar.gz | tar xvzf - -C ./mdfmt/ && mv ./mdfmt/mdfmt ./pandoc-2.19.2/bin/ && rm -rf ./mdfmt
+        # set $PATH
+        export PATH="$(pwd)/pandoc-2.19.2/bin:$PATH"
+    fi
 else
-	echo "You are use Windows now."
+    echo "You are use Windows now."
 fi
 
 # init nodejs-lts
 if [ "$(uname -s)" = "Linux" ] && test ! -d $HOME/.n; then
-	echo -e "\e[36m init nodejs-lts \e[0m"
-	export N_PREFIX=$HOME/.n
-	export PATH="$HOME/.n/bin:$PATH"
-	bash <(curl -L https://raw.gitmirror.com/tj/n/master/bin/n) lts
+    echo -e "\e[36m init nodejs-lts \e[0m"
+    export N_PREFIX=$HOME/.n
+    export PATH="$HOME/.n/bin:$PATH"
+    bash <(curl -L https://raw.gitmirror.com/tj/n/master/bin/n) lts
 fi
 
 if hash node &>/dev/null; then
-	echo "already have nodejs here."
+    echo "already have nodejs here."
 fi
 
 if hash pnpm 2>/dev/null; then
-	echo 'already have pnpm here.'
+    echo 'already have pnpm here.'
 else
-	npm install -g pnpm
+    npm install -g pnpm
 fi
 
 pnpm install
+
+# gen
+
+curl http://127.0.0.1:41184 >/dev/null 2>&1 && echo -e "\e[36m pnpm mami ... \e[0m" && pnpm mami
+
+# mdfmt
+echo -e "\e[36m do mdfmt ... \e[0m"
+for i in $(ls source/_posts); do mdfmt -w source/_posts/$i; done
+
+if [ "$(uname -s)" = "Linux" ]; then
+    echo "You are use Linux now."
+else
+    echo -e "\e[36m Widows: LF --> CRLF \e[0m"
+    for i in $(ls source/_posts); do sed -i 's/$/\r/g' source/_posts/$i; done
+fi
 
 # build blog
 echo -e "\e[36m build blog \e[0m"
